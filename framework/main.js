4bc84b1e02e4b31ee5982cdbf00e20ec4b471f45
prooxeydev <louis1411@gmail.com>
2020-03-25 16:27:03 +0100

    Changed main.js

--- framework/main.js
+++ framework/main.js
@@ -1,25 +1,19 @@
 import { StyleManager } from "./modules/manager/styleManager.js";
 
-window.addEventListener('load', () => {
-    main();
-});
-
 var styleManager;
 
-function main() {
-    document.getElementById('new').addEventListener('click', () => {
-        let elementName = prompt('Element name', 'div');
-        if (elementName != null) {
-            newElement(elementName);
-        }
-    });
-    document.getElementById('addStyle').addEventListener('click', () => {
-        let styleName = prompt('Style name', 'width');
-        if (styleName != null) {
-            styleManager.style(styleName, '0');
-        }
-    });
-}
+document.getElementById('new').addEventListener('click', () => {
+    let elementName = prompt('Element name', 'div');
+    if (elementName != null) {
+        newElement(elementName);
+    }
+});
+document.getElementById('addStyle').addEventListener('click', () => {
+    let styleName = prompt('Style name', 'width');
+    if (styleName != null) {
+        styleManager.style(styleName, '0');
+    }
+});
 
 function newElement(elementName) {
     let element = document.createElement(elementName);
