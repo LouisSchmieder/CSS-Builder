950c3dac150212ed8429bf3cecb90ceb1707d467
prooxeydev <louis1411@gmail.com>
2020-03-25 16:56:34 +0100

    fixed styleManager.js

--- framework/modules/manager/styleManager.js
+++ framework/modules/manager/styleManager.js
@@ -8,6 +8,36 @@ class StyleManager {
     style(styleName, value) {
         this.element.style[styleName] = value;
         this.styleData[styleName] = this.element.style[styleName];
+
+        if(this.styleData['width'] != null) {
+            let left;
+            let width;
+            let format = '';
+            if(this.styleData['width'].endsWith('px')) {
+                width = parseInt(this.styleData['width'].replace('px', ''));
+                format = 'px';
+            } else if(this.styleData['width'].endsWith('%')) {
+                width = parseInt(this.styleData['width'].replace('%', ''));
+                format = '%';
+            }
+            left = window.screen.availWidth / 2 - width / 2;
+            this.element.style.left = left + '' + format;
+        }
+        if(this.styleData['height'] != null) {
+            let top;
+            let height;
+            let format = '';
+            if(this.styleData['height'].endsWith('px')) {
+                height = parseInt(this.styleData['height'].replace('px', ''));
+                format = 'px';
+            } else if(this.styleData['width'].endsWith('%')) {
+                height = parseInt(this.styleData['height'].replace('%', ''));
+                format = '%';
+            }
+            top = window.screen.availHeight / 2 - height / 2;
+            this.element.style.top = top + format;
+        }
+
         if (document.getElementById(styleName) === null) {
             this.addNewToList(styleName);
         }
